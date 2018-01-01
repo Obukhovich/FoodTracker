9d169dd4fdf88490aec347fb6d18f878dcd17e96
Arthur Obukhovich <arthur.obukhovich@gmail.com>
2018-01-01 18:05:06 +0300

    add RatingControl.swift and Horizontal Stack View object

---
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -0,0 +1,22 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Arthur Obukhovich on 1/1/18.
+//  Copyright © 2018 Артур Обухович. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+    
+    //MARK: Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
